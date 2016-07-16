7fbcc1d5de6084ba252b8136407f50d9d55843e6
m358807551 <m358807551@163.com>
2016-07-16 21:19:31 +0800

    [features]_update_google

--- qpalace/common/models.py
+++ qpalace/common/models.py
@@ -75,3 +75,13 @@ class MGit(models.Model):
         return self.question
     
 admin.site.register(MGit)
+
+
+class MGoogle(models.Model):
+    question = models.TextField()
+    solution = models.TextField()
+
+    def __unicode__(self):
+        return self.question
+    
+admin.site.register(MGoogle)
